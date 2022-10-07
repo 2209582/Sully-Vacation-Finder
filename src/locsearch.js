8c5634ce55e40cd55b15ae344136735857dc69f2
2209582 <115106209+2209582@users.noreply.github.com>
2022-10-07 11:04:39 -0600

    added functionality for randLocSearchButton

--- src/locsearch.js
+++ src/locsearch.js
@@ -14,49 +14,7 @@ const locSelect = document.getElementById("categoryDropdown");
 let selectVal = "placeholder";
 let locText = document.getElementById("locText");
 const locSearchButton = document.getElementById("locSearchButton");
-const randLocSearchButton = documet.getElementById("randLocSearchButton");
-
-setInterval(() => {
-  if (locSelect && locText) {
-    selectVal = locSelect.value;
-  }
-}, 500);
-
-locSearchButton.addEventListener("click", () => {
-  locText.textContent = `Places in category: ${selectVal}`;
-  
-randLocSearchButton.addEventListener("click", () => {
-  locText.textContent = `Random Place in category: ${selectVal}`;
-
-  let currCategory = [];
-  let currImgs = [];
-  switch (selectVal) {
-    case "Cheap":
-      currCategory = cheapLocs;
-      currImgs = cheapImgs;
-      break;
-    case "Historical":
-      currCategory = historicalLocs;
-      currImgs = historicalImgs;
-      break;
-    case "Lakes/Glaciers":
-      currCategory = lakeLocs;
-      currImgs = lakeImgs;
-      break;
-    case "Fun for the Family":
-      currCategory = familyLocs;
-      currImgs = familyImgs;
-      break;
-    default:
-      currCategory = "error";
-      break;
-  }
-
-  for (let i = 0; i < 3; i++) {
-    locTexts[i].textContent = currCategory[i];
-    locImgs[i].src = currImgs[i];
-  }
-});
+const randLocSearchButton = document.getElementById("randLocSearchButton");
 
 let cheapLocs = [];
 let cheapImgs = [];
@@ -70,6 +28,7 @@ for (let i = 0; i < locations.historicalIndices.length; i++) {
   historicalLocs.push(locations.locs[locations.historicalIndices[i]]);
   historicalImgs.push(locations.imgSrcs[locations.historicalIndices[i]]);
 }
+
 let lakeLocs = [];
 let lakeImgs = [];
 for (let i = 0; i < locations.lakeIndices.length; i++) {
@@ -82,3 +41,56 @@ for (let i = 0; i < locations.familyIndices.length; i++) {
   familyLocs.push(locations.locs[locations.familyIndices[i]]);
   familyImgs.push(locations.imgSrcs[locations.familyIndices[i]]);
 }
+
+let currCategory = [];
+let currImgs = [];
+setInterval(() => {
+  if (locSelect && locText) {
+    selectVal = locSelect.value;
+    switch (selectVal) {
+      case "Cheap":
+        currCategory = cheapLocs;
+        currImgs = cheapImgs;
+        break;
+      case "Historical":
+        currCategory = historicalLocs;
+        currImgs = historicalImgs;
+        break;
+      case "Lakes/Glaciers":
+        currCategory = lakeLocs;
+        currImgs = lakeImgs;
+        break;
+      case "Fun for the Family":
+        currCategory = familyLocs;
+        currImgs = familyImgs;
+        break;
+      default:
+        currCategory = "error";
+        break;
+    }
+  }
+}, 500);
+
+locSearchButton.addEventListener("click", () => {
+  locText.textContent = `Places in category: ${selectVal}`;
+  for (let i = 0; i < 3; i++) {
+    if (locTexts[i] && locImgs[i]) {
+      locTexts[i].textContent = currCategory[i];
+      locImgs[i].src = currImgs[i];
+    }
+  }
+});
+
+randLocSearchButton.addEventListener("click", () => {
+  locText.textContent = `Random Place in category: ${selectVal}`;
+
+  if (locTexts[0] && locImgs[0]) {
+    for (let i = 1; i < 3; i++) {
+      locTexts[i].textContent = "";
+      locImgs[i].src = "";
+    }
+    const randNum = Math.floor(Math.random() * 3);
+    locTexts[0].textContent = currCategory[randNum];
+    locImgs[0].src = currImgs[randNum];
+  }
+});
