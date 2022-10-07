571e6555a95593e58dad9306dc8245b65d0be469
2209582 <115106209+2209582@users.noreply.github.com>
2022-10-07 11:02:50 -0600

    fixed bug

--- src/randloc.js
+++ src/randloc.js
@@ -4,10 +4,10 @@ let randLocText = document.getElementById("randLocText");
 let randLocImg = document.getElementById("randLocImg");
 
 function genRandLoc() {
-  const randNum = Math.floor(Math.random() * 9);
+  const randNum = Math.floor(Math.random() * 8);
   if (randLocText) {
     randLocText.textContent = locations.locs[randNum];
-    if (randImg) {
+    if (randLocImg) {
       randLocImg.src = locations.imgSrcs[randNum];
     }
   }
